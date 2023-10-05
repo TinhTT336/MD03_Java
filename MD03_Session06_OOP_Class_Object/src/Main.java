2130a36fbb9c607a4e4be5c2c1e1fb9716a8b136
TinhTT336 <138435713+TinhTT336@users.noreply.github.com>
2023-10-05 20:50:52 +0700

    Add files via upload

--- MD03_Session06_OOP_Class_Object/src/Main.java
+++ MD03_Session06_OOP_Class_Object/src/Main.java
@@ -1,10 +1,3 @@
-import MD03_Session06_OOP_Class_Object_Lession.*;
-import MD03_Session06_Practice.Calculator;
-import MD03_Session06_Practice.Category;
-import MD03_Session06_Practice.Product;
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
